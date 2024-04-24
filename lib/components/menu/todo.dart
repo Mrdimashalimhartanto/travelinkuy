838e5627d005a353e555d647eff66514a91d4325
Dimas Halim Hartanto <dimashalimgit@gmail.com>
2024-04-24 17:56:58 +0700

    update todo

--- lib/components/menu/todo.dart
+++ lib/components/menu/todo.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, prefer_final_fields, library_private_types_in_public_api, unnecessary_import, use_key_in_widget_constructors, avoid_unnecessary_containers
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, prefer_final_fields, library_private_types_in_public_api, unnecessary_import, use_key_in_widget_constructors, avoid_unnecessary_containers, unused_field
 
 part of 'menu.dart';
 
